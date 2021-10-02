7ad506f5a335c2bfad37c25b80bb3633079c5d8e
unknown <khomanesuraj8@gmail.com>
2021-10-02 20:27:00 +0530

    generating random die number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,12 @@
+import java.util.*;
 public class SnakeAndLadder{
 public static void main(String[] args){
 
 	System.out.println("Lets play");
 
+	Random rand =new Random();
+	int dice = rand.nextInt(6) + 1;
+	System.out.println(dice);
+	
    }
 }
