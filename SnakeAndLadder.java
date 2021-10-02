45705cd4b64fdc4314b910e03d0433a147a3a7d6
unknown <khomanesuraj8@gmail.com>
2021-10-02 23:20:02 +0530

    Added player option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,7 +1,40 @@
+import java.util.*;
 public class SnakeAndLadder{
 public static void main(String[] args){
 
 	System.out.println("Lets play");
+	rollDice();
+	playerOption();
 
    }
+public static int rollDice(){
+	Random rand = new Random();
+	int dice = rand.nextInt(6) + 1;
+	return dice;
+    }
+public static void playerOption(){
+	int position=0;
+	//random values for option
+	int check =(int)(Math.random() * 10) % 3 + 1;
+	System.out.println("check "+check);
+
+	int dice=rollDice();
+	System.out.println("Dice number "+dice);
+
+	switch(check){
+		//case for no play
+		case 1:
+			System.out.println("player is not playing "+position);
+			break;	
+		// case for player position after play
+		case 2:
+			position = position + dice;
+			System.out.println("player is playing "+position);
+			break;
+		//case for snake , player postion 
+		default:
+			position = position - dice; 
+			System.out.println("player is  "+position); 
+      		}
+	}
 }
